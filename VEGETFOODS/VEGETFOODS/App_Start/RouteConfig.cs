e432013182bad495bc2e6e71f145e8f3bc212e73
MyVanBui <myvanbui6112000@gmail.com>
2022-03-06 10:18:16 +0700

    init project

---
+++ VEGETFOODS/VEGETFOODS/App_Start/RouteConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace VEGETFOODS
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+               name: "Default1",
+               url: "home-page",
+               defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+               namespaces: new[] { "VEGETFOODS.Controllers.MVC_Controller" }
+           );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "VEGETFOODS.Controllers.MVC_Controller" }
+            );
+        }
+    }
+}
